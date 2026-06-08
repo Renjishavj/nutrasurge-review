320cf7b124c9a0074b8e6d57b67a1f4efe34f7f5
Renjishavj <renjishavj@gmail.com>
2026-06-08 14:09:59 +0530

    seo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 import AdminLogin from './components/AdminLogin';
 import Dashboard from './components/Dashboard';
 import Home from './components/Home';
@@ -6,15 +7,23 @@ import ProductDetails from './components/ProductDetails';
 import './index.css';
 
 function App() {
+  const isRenderDomain = window.location.hostname.endsWith('.onrender.com');
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/nutrasurge-login" element={<AdminLogin />} />
-        <Route path="/admin" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <>
+      <Helmet>
+        <link rel="canonical" href="https://www.nutrasurgelabsreviews.com" />
+        {isRenderDomain && <meta name="robots" content="noindex,nofollow" />}
+      </Helmet>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/nutrasurge-login" element={<AdminLogin />} />
+          <Route path="/admin" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
